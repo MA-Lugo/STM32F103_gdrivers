90abf800db10fd3965ce3129bcb3fc190bf32d24
Mario A. Lugo <mario.luggar@gmail.com>
2021-01-25 18:32:58 -0600

    feat(spi) Implements some APIs
    Implements SPI_CLK_Control API for configure the clock
    of the given SPI port.
    
    Implements SPI_DeInit for De-initialize the registers
    of the given SPI port.

--- Src/stm32f103_spi_driver.c
+++ Src/stm32f103_spi_driver.c
@@ -6,10 +6,54 @@
  */
 #include "stm32f103_spi_driver.h"
 
-
+/**********************************************************
+ * @fn				- SPI_CLK_Control
+ * @brief			- This function enable or disable the peripheral clock
+ * 					  for the given SPIx
+ *
+ * @param[in]		- Base addres of the SPI port
+ * @param[in]		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+ * @note			- none
+ *********************************************************/
 
 void SPI_CLK_Control(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
 {
+	if (EnOrDi == ENABLE)
+	{
+		if (pSPIx == SPI1)
+		{
+			SPI1_CLK_ENABLE();
+		}
+		else if (pSPIx == SPI2)
+		{
+			SPI2_CLK_ENABLE();
+		}
+		else if (pSPIx == SPI3)
+		{
+			SPI3_CLK_ENABLE();
+		}
+
+	}
+
+	else
+	{
+		if (pSPIx == SPI1)
+		{
+			SPI1_CLK_DISABLE();
+		}
+		else if (pSPIx == SPI2)
+		{
+			SPI2_CLK_DISABLE();
+		}
+		else if (pSPIx == SPI3)
+		{
+			SPI3_CLK_DISABLE();
+		}
+
+	}
 
 }
 
@@ -17,8 +61,35 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 {
 
 }
+
+
+/**********************************************************
+ * @fn				- SPI_DeInit
+ * @brief			- This function De-Init the given SPI Port
+ *
+ *
+ * @param[in]		- Base addres of the SPIx
+ *
+ * @return			- none
+ *
+ * @note			- none
+ *********************************************************/
 void SPI_DeInit(SPI_RegDef_t *pSPIx)
 {
+
+	if (pSPIx == SPI1)
+	{
+		SPI1_REG_RESET();
+	}
+	else if (pSPIx == SPI2)
+	{
+		SPI2_REG_RESET();
+	}
+	else if (pSPIx == SPI3)
+	{
+		SPI3_REG_RESET();
+	}
+
 
 }
 
