dee25bc9f5714542fde4d4b40ec6038e0fa31df9
Mario A. Lugo <mario.luggar@gmail.com>
2021-03-06 01:57:23 -0600

    ed

---
+++ Inc/stm32f103_i2c_driver.h
@@ -0,0 +1,15 @@
+/*
+ * stm32f103_i2c_driver.h
+ *
+ *  Created on: 6 mar. 2021
+ *      Author: Mario
+ */
+
+#ifndef INC_STM32F103_I2C_DRIVER_H_
+#define INC_STM32F103_I2C_DRIVER_H_
+
+#include "stm32f103xx.h"
+
+
+
+#endif /* INC_STM32F103_I2C_DRIVER_H_ */
---
+++ Src/stm32f103_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f103_i2c_driver.c
+ *
+ *  Created on: 6 mar. 2021
+ *      Author: Mario
+ */
+
+#include "stm32f103_i2c_driver.h"
