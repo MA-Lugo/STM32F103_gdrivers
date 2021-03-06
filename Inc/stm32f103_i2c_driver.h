283e458807985838f9da2251a0023ae45c929256
Mario A. Lugo <mario.luggar@gmail.com>
2021-03-06 13:15:37 -0600

    feat(i2c): Implement some APIs
    Imlement I2C_CLK_Control, I2C_DeInit and
    I2C_PeripheralControl APIs

--- Inc/stm32f103_i2c_driver.h
+++ Inc/stm32f103_i2c_driver.h
@@ -11,5 +11,76 @@
 #include "stm32f103xx.h"
 
 
+typedef struct
+{
+	uint32_t I2C_SCLSpeed;
+	uint8_t  I2C_DeviveAddres;
+	uint8_t  I2C_ACKControl;
+	uint16_t I2C_FMDutyCycle;
+}I2C_Config_t;
+
+
+/*
+ * Handle structure for I2Cx peripheral
+ */
+
+typedef struct
+{
+	I2C_RegDef_t *pI2Cx;
+	I2C_Config_t I2C_Config;
+}I2C_Handle_t;
+
+
+/*
+ * I2C_SCLSpeed macros
+ */
+#define I2C_SCL_SPEED_SM			100000
+#define I2C_SCL_SPEED_FM400K		400000
+#define I2C_SCL_SPEED_FM200K		200000
+
+/*
+ * I2C_ACKControl macros
+ */
+
+#define I2C_ACK_ENABLE				1
+#define I2C_ACK_DISABLE				0
+
+/*
+ * I2C_FMDutyCycle macros
+ */
+#define I2C_FM_DUTY_2				0
+#define I2C_FM_DUTY_16_9			1
+
+
+
+/****************************************************************************************************************
+ * 						APIs supported by this driver
+ *****************************************************************************************************************/
+
+
+/*
+ * Pheriperal SETUP
+ */
+void I2C_CLK_Control(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDis);
+
+/*
+ * Send and Receive Data
+ */
+
+/*
+ * IRQ configuration and ISR handling
+ */
+
+void I2C_IRQInterrupt_Config(uint8_t IRQNumber, uint8_t EnorDi);
+void I2C_IRQPiority_Config(uint8_t IRQNumber, uint32_t IRQPriority);
+
+/*
+ * Others
+ */
 
 #endif /* INC_STM32F103_I2C_DRIVER_H_ */
--- Src/stm32f103_i2c_driver.c
+++ Src/stm32f103_i2c_driver.c
@@ -6,3 +6,96 @@
  */
 
 #include "stm32f103_i2c_driver.h"
+
+
+/**********************************************************
+ * @fn				- I2C_CLK_Control
+ * @brief			- This function enable or disable the peripheral clock
+ * 					  for the given I2Cx
+ *
+ * @param[in]		- Base addres of the I2C port
+ * @param[in]		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+ * @note			- none
+ *********************************************************/
+
+void I2C_CLK_Control(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi)
+{
+	if (EnOrDi == ENABLE)
+	{
+		if (pI2Cx == I2C1)
+		{
+			I2C1_CLK_ENABLE();
+		}
+		else if (pI2Cx == I2C2)
+		{
+			I2C2_CLK_ENABLE();
+		}
+
+	}
+
+	else
+	{
+		if (pI2Cx == I2C1)
+		{
+			I2C1_CLK_DISABLE();
+		}
+		else if (pI2Cx == I2C2)
+		{
+			I2C2_CLK_DISABLE();
+		}
+
+	}
+
+}
+
+/**********************************************************
+ * @fn				- I2C_DeInit
+ * @brief			- This function De-Init the given I2C Port
+ *
+ *
+ * @param[in]		- Base addres of the I2Cx
+ *
+ * @return			- none
+ *
+ * @note			- none
+ *********************************************************/
+void I2C_DeInit(I2C_RegDef_t *pI2Cx)
+{
+
+	if (pI2Cx == I2C1)
+	{
+		I2C1_REG_RESET();
+	}
+	else if (pI2Cx == I2C2)
+	{
+		I2C2_REG_RESET();
+	}
+
+}
+
+/**********************************************************
+ * @fn				- I2C_PeripheralControl
+ * @brief			- This function enable or disable the peripheral
+ *
+ *
+ * @param[in]		- Base addres of the I2Cx
+ * @param[in]		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+ * @note			-
+ *********************************************************/
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDis)
+{
+	if(EnOrDis == ENABLE)
+	{
+		pI2Cx->CR1 |= (1 << I2C_CR1_PE);
+	}
+	else
+	{
+		pI2Cx->CR1 &= ~(1 << I2C_CR1_PE);
+	}
+}
